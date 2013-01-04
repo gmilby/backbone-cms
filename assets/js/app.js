d3fe849f822ad04cfa07fd928310691e0839af4b
Tyler Sargent <tsargent@gmail.com>
2013-01-04 10:04:38 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,3 @@
 $(document).ready(function(){
     
-    Page = Backbone.Model.extend({
-        initialize: function() {
-            console.log('Page model initialized')
-        }
-    });
-    
-    Pages = Backbone.Collection.extend({
-        model: Page
-    });
-    
-    PageView = Backbone.View.extend({
-        
-        el: $('#container'),
-        
-        template: _.template($('#page-template').html()),
-        
-        initialize: function(){
-            this.model.on( 'change', this.render, this );
-            this.model.on( 'destroy', this.remove, this );
-        },
-        
-        render: function(){
-            this.$el.html( this.template( this.model.toJSON() ) );
-            return this;
-        }
-    });
-    
-    
-    
-    
 });
